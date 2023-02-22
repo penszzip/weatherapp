bdc2fd5165c869a6fc93ae62f92986226cf1bf63
penszzip <76844478+penszzip@users.noreply.github.com>
2023-02-22 10:42:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('#weatherForm');
+const input = document.querySelector('#locationInput');
+const forecast = document.querySelector('.forecast')
+
+form.addEventListener('submit', async function (e) {
+    try {
+        e.preventDefault();
+        if (document.querySelector('p')) {
+            reset()
+            forecast.classList.remove('pb-1')
+        }
+
+        const location = input.value;
+        console.log(location)
+        const coordinates = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${location}`)
+        console.log(coordinates)
+        const { latitude, longitude } = coordinates.data.results[0]
+        
+        const weather = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
+        const { current_weather } = weather.data
+        
+        input.value = ''
+        
+        const label = document.createElement('p')
+        label.classList.add('h2')
+        label.innerText = location
+        const temperature = document.createElement('p')
+        temperature.classList.add('h1')
+        temperature.classList.add('temperature')
+        temperature.innerText = `${current_weather.temperature} \u2103`
+        const windSpeed = document.createElement('p')
+        windSpeed.classList.add('h3')
+        windSpeed.classList.add('windSpeed')
+        windSpeed.innerText = `Wind speed: ${current_weather.windspeed} km/h`
+
+        forecast.append(label)
+        forecast.append(temperature)
+        forecast.append(windSpeed)
+        forecast.classList.add('pb-1')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+function reset() {
+    const disposeItems = document.querySelectorAll('p')
+    disposeItems.forEach(item => {
+        item.remove()
+    })
+}
